9a9fe075be0dca03ef449ac76238c649e0e3e901
Tvoje Ime <tvoj@email.com>
2025-11-01 14:25:18 +0100

    chore: remove temporary batch file

--- commit-and-push.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "c:\PAU_ALAC\Ani's Studio\anis-studio"
-git add .
-git commit -m "feat: implement all design improvements - compact layout and symmetrical buttons"
-git push
-pause
-
